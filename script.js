1bc1b716cfb9bad521ed7cdaa26778b99cb991aa
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-09-17 02:16:42 +0600

    90% bug is now belai ha ha ha

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -137,3 +137,12 @@ setInterval(() => {
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
+
+
+function goToBlog() {
+  window.location.href = "/blog.html";
+}
+
+function goToHome() {
+  window.location.href = "/index.html";
+}
